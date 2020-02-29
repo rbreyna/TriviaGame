b6241429fc44d3261121289495772d8e1b632a3c
Roy Reyna <royreyna7@yahoo.com>
2020-02-28 20:04:19 -0600

    questions set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#start").click(function(){
+        $("#container").css("visibility","visible");
+    });
+  
+  });
+
+
+/* $("#start").click(function(){
+    setInterval(function(){
+        $(".container").css.visibility="visible";
+    },120*1000);
+}); */
+
+/* $("#start").click(function(){
+    $(".container").attr("visibility","visible");
+}); */
