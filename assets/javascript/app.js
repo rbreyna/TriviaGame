6b705faca57482feab1dd50341898e25b209c680
Roy Reyna <royreyna7@yahoo.com>
2020-02-29 10:05:05 -0600

    game basic functionality done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,66 @@
 $(document).ready(function () {
 
-    var count = 8;
+    var count = 90;
     var countdown;
     var ready = false;
     var correct = 0;
 
-    $("<button>").click(function () {
+    $("#start").click(function () {
         $("#container").css("visibility", "visible");
         ready = true;
+
+        if (ready) {
+
+            countdown = setInterval(timer, 1000);
+        
+            timer();
+    
+        }
     });
 
-    if (ready) {
-        countdown = setInterval(timer, 1000);
+    function timer() {
+        count--;
+        console.log(count);
+        var conversion = timeConverter(count);
 
-        function timer() {
-            count--;
-
-            var conversion = timeConverter(count);
-
-            $("#timer").html("Time Remaining: " + conversion);
-        }
-
-        function timeConverter(time) {
-            if (time == 0) {
-                clearInterval(countdown);
-                alert("Time is up!");
-                ready = false;
-            }
-
-            if (time > 59) {
-                var minutes = "01";
-                var seconds = time - 60;
-
-                if (seconds < 10) {
-
-                    seconds = "0" + seconds;
-                }
-
-                return minutes + ":" + seconds;
-            }
-
-            {
-                var minutes = "00";
-                var seconds = time;
-
-                if (seconds < 10) {
-                    seconds = "0" + seconds;
-                }
-
-                return minutes + ":" + seconds;
-            }
-
-
-        }
+        $("#timer").html("Time Remaining: " + conversion);
     }
+    
+    function timeConverter(time) {
+        if (time == 0) {
+            clearInterval(countdown);
+            alert("Time is up!");
+            ready = false;
+        }
+
+        if (time > 59) {
+            var minutes = "01";
+            var seconds = time - 60;
+
+            if (seconds < 10) {
+
+                seconds = "0" + seconds;
+            }
+
+            return minutes + ":" + seconds;
+        }
+
+        {
+            var minutes = "00";
+            var seconds = time;
+
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+
+            return minutes + ":" + seconds;
+        }
+
+
+    }
+
+
+
     $("#submit").click(function () {
         var question1 = $('input[name = "response1"]:checked').val();
         var question2 = $('input[name = "response2"]:checked').val();
